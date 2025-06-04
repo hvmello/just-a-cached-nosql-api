b3a8e5eb5bf97fd63b1c4a2155c3b8d855837ce5
Heitor Vieira Mello <heitor29_@hotmail.com>
2025-06-04 19:31:10 -0300

    Initial commit

---
+++ src/test/java/com/crud/market_api/MarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.market_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
