a2ba458156227de2773cb929e8f74db598677de3
Heitor Vieira Mello <heitor29_@hotmail.com>
2025-06-04 19:50:08 -0300

    Add ProductRepository interface

---
+++ src/main/java/com/crud/market_api/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.crud.market_api.repository;
+
+import com.crud.market_api.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
