fc042bf4b4969153fef28eb0e00f88f0db03b811
Heitor Vieira Mello <heitor29_@hotmail.com>
2025-06-04 20:04:54 -0300

    Add centralized exception handling and integrate basic product CRUD operations in ProductService implementation

---
+++ src/main/java/com/crud/market_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+
+package com.crud.market_api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        ErrorResponse error = new ErrorResponse(
+            HttpStatus.NOT_FOUND.value(),
+            ex.getMessage(),
+            System.currentTimeMillis()
+        );
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
